c3e122948d1246628d221be3ac6ee28bda9c7048
brian herman <brian herman>
2017-04-07 22:15:34 -0500

    added function and json

---
+++ function.py
@@ -0,0 +1,16 @@
+import datetime
+
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+
+try:
+    api = Connection(appid="")
+    response = api.execute('findItemsAdvanced',{'keywords':'legos'})
+
+except ConnectionError as e:
+    print(e)
+    print(e.response.dict())
+    
+
+def lambda_handler(event, context):
+    print("Event: %s" % event)
